7beb071a9945a2710a80abcd4143ae276e429556
Arvind Pai <arvindmpai@gmail.com>
2023-09-09 19:15:00 -0700

    Initial Commit

--- examples/main.tf
+++ examples/main.tf
@@ -3,10 +3,11 @@ resource "random_string" "launch_id" {
   special = false
   upper   = false
   }
-  
+
 locals {
   suffix = format("%s-%s", "tf", random_string.launch_id.result)
 }
+
 
 module "bings_bun" {
   source           = "../modules/gce"
